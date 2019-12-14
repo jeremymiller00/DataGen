184e6e80cc7b6c74edc52cfc31522ffef84bb870
Jeremy <jeremymiller00@gmail.com>
2019-12-14 12:52:14 -0800

    update layout

--- src/DataGen.py
+++ src/DataGen.py
@@ -5,4 +5,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 
+class DataGen:
+
+    def new_dataset(self, features: list) -> pd.DataFrame:
+        '''
+        Datasets are structured as Pandas Dataframes
+        A new dataset requires at least one feature
+        '''
+        df = pd.DataFrame(data=features)
+        return df
+
+    def add_feature(self, feature_name, distribution=scs.norm, size=100):
+        pass
+
+    def get_features():
+        pass
+
+    def hist():
+        pass
+
+    def write_data():
+        pass
+
 
