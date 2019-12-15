90a09d185f44d5f6b9acab254e99d8c5635a637b
Jeremy <jeremymiller00@gmail.com>
2019-12-15 07:55:49 -0800

    developing tests

--- test_data_gen.py
+++ test_data_gen.py
@@ -3,19 +3,21 @@ A set of basic tests for the DataGen class
 
 '''
 import unittest
-from src.DataGen import DataGen
+from src.AssortmentDataGenerator import AssortmentDataGenerator
 import pandas as pd
 
 class TestDataGen(unittest.TestCase):
 
     def test_constructor(self):
-        dg = DataGen()
-        self.assertIsInstance(dg, DataGen)
+        data = [(1,2,3),[4,5,6]]
+        names = ["one", "two", "three"]
+        dg = AssortmentDataGenerator(features_data=data, feature_names=names)
+        self.assertIsInstance(dg, AssortmentDataGenerator)
 
-    def test_new_dataset(self):
-        dg = DataGen()
-        data = dg.new_dataset(features = [1,2,3,4,5])
-        self.assertIsInstance(data, pd.DataFrame)
+#    def test_new_dataset(self):
+#        dg = DataGen()
+#        data = dg.new_dataset(features = [1,2,3,4,5])
+#        self.assertIsInstance(data, pd.DataFrame)
 
 if __name__ == '__main__':
     unittest.main()
